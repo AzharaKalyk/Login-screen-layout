52c0184507388f693314924b15a889351b5c169d
AzharaKalyk <kab_180495@mail.ru>
2023-06-19 22:17:22 +0600

    added_button
    Добавила кнопки и дополнение

--- HomeWork11-Kalyk-Azhar/ViewController.swift
+++ HomeWork11-Kalyk-Azhar/ViewController.swift
@@ -59,6 +59,54 @@ class ViewController: UIViewController {
         return textField
     }()
     
+    private lazy var buttonLogin: UIButton = {
+        let buttonLogin = UIButton(type: .system)
+        buttonLogin.setTitleColor(.white, for: .normal)
+        buttonLogin.setTitle("Login", for: .normal)
+        buttonLogin.backgroundColor = .systemBlue
+        buttonLogin.layer.cornerRadius = 20
+        buttonLogin.translatesAutoresizingMaskIntoConstraints = false
+        
+        return buttonLogin
+    }()
+    
+    private lazy var questionLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .darkGray
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.textAlignment = .center
+        label.text = "Forgot your password?"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private lazy var connectLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .darkGray
+        label.font = UIFont.systemFont(ofSize: 10)
+        label.textAlignment = .center
+        label.text = "or connect with"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private lazy var lineView1: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        return view
+    }()
+    
+    private lazy var lineView2: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -79,6 +127,11 @@ class ViewController: UIViewController {
         view.addSubview(stackView)
         stackView.addArrangedSubview(loginText)
         stackView.addArrangedSubview(passwordText)
+        view.addSubview(buttonLogin)
+        view.addSubview(questionLabel)
+        view.addSubview(connectLabel)
+        view.addSubview(lineView1)
+        view.addSubview(lineView2)
     }
     
     private func setupLayout() {
@@ -101,7 +154,30 @@ class ViewController: UIViewController {
             passwordText.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50),
             passwordText.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -50),
             passwordText.widthAnchor.constraint(equalToConstant: 250),
-            passwordText.heightAnchor.constraint(equalToConstant: 40)
+            passwordText.heightAnchor.constraint(equalToConstant: 40),
+            
+            buttonLogin.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            buttonLogin.topAnchor.constraint(equalTo: passwordText.bottomAnchor, constant: 40),
+            buttonLogin.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50),
+            buttonLogin.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -50),
+            buttonLogin.widthAnchor.constraint(equalToConstant: 250),
+            buttonLogin.heightAnchor.constraint(equalToConstant: 40),
+            
+            questionLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            questionLabel.topAnchor.constraint(equalTo: buttonLogin.bottomAnchor, constant: 20),
+            
+            connectLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            connectLabel.bottomAnchor.constraint(equalTo: questionLabel.bottomAnchor, constant: 140),
+            
+            lineView1.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            lineView1.trailingAnchor.constraint(equalTo: connectLabel.leadingAnchor, constant: -10),
+            lineView1.centerYAnchor.constraint(equalTo: connectLabel.centerYAnchor),
+            lineView1.heightAnchor.constraint(equalToConstant: 1),
+            
+            lineView2.leadingAnchor.constraint(equalTo: connectLabel.trailingAnchor, constant: 10),
+            lineView2.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            lineView2.centerYAnchor.constraint(equalTo: connectLabel.centerYAnchor),
+            lineView2.heightAnchor.constraint(equalToConstant: 1)
         ])
     }
 }
