008b4aa73d67735469d5abadfb1b8437485a3050
AzharaKalyk <kab_180495@mail.ru>
2023-06-19 22:06:47 +0600

    added_login_background
    Добавили логин и фон

--- HomeWork11-Kalyk-Azhar/ViewController.swift
+++ HomeWork11-Kalyk-Azhar/ViewController.swift
@@ -1,19 +1,57 @@
-//
-//  ViewController.swift
-//  HomeWork11-Kalyk-Azhar
-//
-//  Created by Azhar Kalyk on 19.06.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: - UI Elements
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Login"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textColor = .darkGray
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let image = UIImage(named: "imageFon")
+        let imageView = UIImageView(image: image)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return imageView
+    }()
+    
+    // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupView()
+        setupHierarchy()
+        setupLayout()
     }
-
-
+    
+    // MARK: - Setups
+    private func setupView() {
+        view.backgroundColor = .white
+    }
+    
+    private func setupHierarchy() {
+        view.addSubview(imageView)
+        view.addSubview(label)
+        
+    }
+    
+    private func setupLayout() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: view.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+        ])
+    }
 }
 
